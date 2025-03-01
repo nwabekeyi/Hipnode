ab98fddca163e1775f4dbb5cf49460b2d3bfdd25
TracyVickin <tracyvickin46@gmail.com>
2025-03-01 13:13:29 +0100

    added context folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { useContext } from "react";
-import { ThemeContext } from "./themeContext";
+import { ThemeContext } from "./context/themeContext";
 
 const App = () => {
   const { theme, toggleTheme, themeColors } = useContext(ThemeContext);
 
+  let buttonStyles = {
+    padding: "10px 20px",
+    border: "none",
+    cursor: "pointer",
+  };
+
+  // Apply different styles for light and dark modes
+  if (theme === "light") {
+    buttonStyles.background = themeColors.buttonColor1;
+    buttonStyles.color = themeColors.buttonColor1Text;
+  } else {
+    buttonStyles.background = themeColors.buttonColor2;
+    buttonStyles.color = themeColors.buttonColor1Text;
+  }
+
   return (
-    <div style={{ background: themeColors.background, color: themeColors.text, padding: "20px", height: "100vh" }}>
+    <div style={{ background: themeColors.background, color: themeColors.textColor, padding: "20px", height: "100vh" }}>
       <h1>Current Theme: {theme}</h1>
-      <button 
-        onClick={toggleTheme} 
-        style={{ background: themeColors.primary, color: "#fff", padding: "10px 20px", border: "none", cursor: "pointer" }}
-      >
+      <button onClick={toggleTheme} style={buttonStyles}>
         Toggle Theme
       </button>
     </div>
--- src/context/themeContext.jsx
+++ src/context/themeContext.jsx
@@ -9,7 +9,6 @@ export const ThemeProvider = ({ children }) => {
   const lightTheme = {
     background: "#ffffff",
     textColor: "#3f4354",
-    primaryColor: "#",
     secondaryColor: "#dcdfe1",
     navbarColor: "#d1daeb",
     clickedNavbarColor: "#ff4400",
@@ -18,7 +17,7 @@ export const ThemeProvider = ({ children }) => {
     label: "#666977",
     placeholder: "#dcdfe1",
     placeholder2: "#dcdfe1",
-    buttonColor1: "#ff8f66",
+    buttonColor1: "#ff6833",
     buttonColor1Text: "#f8f2f1",
     buttonColor2: "#3479e2",
     buttonColor2Text: "#ffffff",
@@ -31,7 +30,6 @@ export const ThemeProvider = ({ children }) => {
   const darkTheme = {
     background: "#1d252a",
     textColor: "#f7f7f7",
-    primaryColor: "#",
     secondaryColor: "#e2e5e7",
     navbarColor: "#f3f6f8",
     clickedNavbarColor: "#ff4400",
@@ -39,7 +37,7 @@ export const ThemeProvider = ({ children }) => {
     placeholder: "#dcdfe1",
     placeholder2: "#52575c",
     buttonColor1Text: "#f8f2f1",
-    buttonColor2: "#3479e2",
+    buttonColor2: "#ff6833",
     buttonColor2Text: "#ffffff",
     lightOrangeColor: "#ffece6",
     lightOrangeColorText: "#ff8f66",
